762245e4a76a1e697ecf18a9101f20d3ba5e2890
Julio Cesar Ledo Amaral Filho <jclafi@gmail.com>
2016-05-16 17:59:37 -0300

    ATZ

--- src/classesbasicas/OlaMundo.java
+++ src/classesbasicas/OlaMundo.java
@@ -1,10 +1,12 @@
 package classesbasicas;
 
+import java.util.concurrent.TimeUnit;
+
 public class OlaMundo {
 
 	public static void main(String[] args) {
 		/* Bloco de Comentário
-		 * Cofrme especificado
+		 * Coforme especificado
 		 * Pela Apostila
 		 * Java */
 		
@@ -13,7 +15,14 @@ public class OlaMundo {
 		/** Comentário Java */
 		
 		System.out.println("Olá Mundo");
-
+		
+		// If the connection failed wait a minute and try again
+		try {
+			 TimeUnit.MINUTES.sleep(1);
+		 }
+		 catch (InterruptedException ex) {
+			 System.out.println("Exceção ao rodar sleep: " + ex.getMessage());
+		 }
 	}
 
 }
